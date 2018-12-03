7e2ecbfb7cbed4a1401c54ed21d6636fe781a64e
Igor Sudenko <sudenkoweb@gmail.com>
2018-12-03 12:12:00 +0200

    Update 2018-12-03T10:11:58.139Z

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 $(function() {
+	jQuery.fn.commonAncestor = function() {
+		var parents = [];
+		var minlen = Infinity;
+	
+		$(this).each(function() {
+			var curparents = $(this).parents();
+			parents.push(curparents);
+			minlen = Math.min(minlen, curparents.length);
+		});
+	
+		for (var i in parents) {
+			parents[i] = parents[i].slice(parents[i].length - minlen);
+		}
+	
+		// Iterate until equality is found
+		for (var i = 0; i < parents[0].length; i++) {
+			var equal = true;
+			for (var j in parents) {
+				if (parents[j][i] != parents[0][i]) {
+					equal = false;
+					break;
+				}
+			}
+			if (equal) return $(parents[0][i]);
+		}
+		return $([]);
+	}
+
 	var ifClick = false;
+	var replySingle = $('.feedback__item .feedbacks__reply');
+
+	replySingle.click(function() {
+		console.log('111');
+	});
 
 	$('.feedback__rate').mouseover(function () { 
 		$(this).addClass('feedback__rate--active').prevAll().addClass('feedback__rate--active');
@@ -19,15 +52,20 @@ $(function() {
 		}
 	});
 
-	$('.feedbacks__reply').click(function() {
-		$(this).next().next().toggle('.feedbacks__reply-content--show');
+	$('.feedbacks__reply').click(function(target) {
+		$('.feedbacks__reply-content', target.currentTarget.offsetParent).toggle('.feedbacks__reply-content--show');
 	});
 
 	$('.feedbacks__replies').mouseover(function() {
-		$(this).addClass('feedbacks__replies--hover');
+		if ( !$(this).hasClass('feedbacks__replies--full') ) {
+			$(this).addClass('feedbacks__replies--hover');
+		}
+		
 	});
 
 	$('.feedbacks__replies').mouseout(function() {
-		$(this).removeClass('feedbacks__replies--hover');
+		if ( !$(this).hasClass('feedbacks__replies--full') ) {
+			$(this).removeClass('feedbacks__replies--hover');
+		}
 	});
 });
